1261f106041877eb1f8f0aa1015c9598bc60ffed
stepazel <stepan.zelenka@seznam.cz>
2025-09-10 16:54:19 +0200

    moving some files

--- Meetings/MeetingRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace Calendar.Meetings;
-
-public class MeetingRequest
-{
-    public int DurationMinutes { get; set; }
-    public DateTime StartTime { get; set; }
-    public string ParticipantFirstName { get; set; } = string.Empty;
-    public string ParticipantLastName { get; set; } = string.Empty;
-}
